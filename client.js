90d15d9486e38e5689e579bbfaa4ba758f521c65
BennyNoyman <bn0504127633@gmail.com>
2023-08-13 15:43:02 +0300

    exercise1.2

--- client.js
+++ client.js
@@ -3,17 +3,26 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const image = document.getElementById('image');
 const age = document.getElementById('age');
-const button = document.getElementById('button');
+const button1 = document.getElementById('button1');
 async function getUserInformation() {
     const user = await fetch('https://randomuser.me/api', {method:'get'});
     return await user.json();
 }
-button.addEventListener('click', async () => {
+button1.addEventListener('click', async () => {
     const userData = await getUserInformation();
     const user = userData.results[0];
-    console.log(user);
     name.textContent = user.name.first+user.name.last;
     email.textContent = user.email;
     image.src = user.picture.large;
     age.textContent = user.dob.age;
+})
+//2
+async function get5male() {
+    const user = await fetch('https://randomuser.me/api?gender=male&results=5', {method: 'get'});
+    return await user.json();
+}
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', async () => {
+    const fiveMales = await get5male();
+    console.log(fiveMales);
 })
